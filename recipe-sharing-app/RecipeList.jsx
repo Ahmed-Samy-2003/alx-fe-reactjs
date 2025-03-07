b296bbc2706b3c7b3e27a9228f6a0ef8f5a057ac
Ahmed-Samy-2003 <aahmedsamy222@gmail.com>
2025-03-07 16:23:40 +0200

    Update RecipeList.jsx

--- recipe-sharing-app/RecipeList.jsx
+++ recipe-sharing-app/RecipeList.jsx
@@ -1,19 +1,13 @@
-// src/components/RecipeList.jsx  
-import { useRecipeStore } from '../recipeStore';  
+import React from 'react';  
 
-const RecipeList = () => {  
-  const recipes = useRecipeStore(state => state.recipes);  
-
-  return (  
-    <div>  
-      {recipes.map(recipe => (  
-        <div key={recipe.id}>  
-          <h3>{recipe.title}</h3>  
-          <p>{recipe.description}</p>  
-        </div>  
-      ))}  
-    </div>  
-  );  
+const RecipeList = ({ recipes }) => {  
+    return (  
+        <ul>  
+            {recipes.map((recipe, index) => (  
+                <li key={index}>{recipe.name}</li>  
+            ))}  
+        </ul>  
+    );  
 };  
 
 export default RecipeList;  

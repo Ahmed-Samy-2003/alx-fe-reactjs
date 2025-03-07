8e17b61b44e81aac1c866302247f02e0a86d85d1
Ahmed-Samy-2003 <aahmedsamy222@gmail.com>
2025-03-07 14:35:52 +0200

    Update WelcomeMessage.jsx

--- alx-react-app/WelcomeMessage.jsx
+++ alx-react-app/WelcomeMessage.jsx
@@ -1,18 +1,6 @@
 function WelcomeMessage() {  
     return (  
         <div>  
-            <h1>Welcome to ALX React APP!</h1>  
-            <p>This is a simple JSX component.</p>  
-        </div>  
-    );  
-}  
-
-export default WelcomeMessage;  
-<h1>Hello everyone, I am learning React at ALX!</h1>  
-<p>I am learning about JSX!</p>  
-function WelcomeMessage() {  
-    return (  
-        <div>  
             <h1>Hello everyone, I am learning React at ALX!</h1>  
             <p>This is a simple JSX component.</p>  
             <p>I am learning about JSX!</p>  

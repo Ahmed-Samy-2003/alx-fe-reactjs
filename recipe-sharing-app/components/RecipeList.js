7f3cedbd30d16ee579b0a7c2ff99d01303b04c15
Ahmed-Samy-2003 <aahmedsamy222@gmail.com>
2025-03-07 16:21:41 +0200

    Update RecipeList.js

--- recipe-sharing-app/components/RecipeList.js
+++ recipe-sharing-app/components/RecipeList.js
@@ -1,1 +1,14 @@
 
+import React from 'react';  
+
+const RecipeList = ({ recipes }) => {  
+    return (  
+        <ul>  
+            {recipes.map((recipe, index) => (  
+                <li key={index}>{recipe.name}</li>  
+            ))}  
+        </ul>  
+    );  
+};  
+
+export default RecipeList;  

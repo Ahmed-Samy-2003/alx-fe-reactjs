2a7b08184feea53c73af4a011e329f558d7f90ed
Ahmed-Samy-2003 <aahmedsamy222@gmail.com>
2025-03-07 15:23:58 +0200

    Update main.jsx

--- form-handling-react/main.jsx
+++ form-handling-react/main.jsx
@@ -9,3 +9,14 @@ ReactDOM.createRoot(document.getElementById('root')).render(
         <RegistrationForm />  
     </React.StrictMode>  
 );  
+import React from 'react';  
+import ReactDOM from 'react-dom/client';  
+import App from './App';  
+import FormikForm from './components/FormikForm';  
+
+ReactDOM.createRoot(document.getElementById('root')).render(  
+    <React.StrictMode>  
+        <App />  
+        <FormikForm />  
+    </React.StrictMode>  
+);  

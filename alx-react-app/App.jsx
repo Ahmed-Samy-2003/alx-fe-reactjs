765abec95f88e0268590b18d2b173f14e92a5643
Ahmed-Samy-2003 <aahmedsamy222@gmail.com>
2025-03-07 14:33:41 +0200

    Create App.jsx

---
+++ alx-react-app/App.jsx
@@ -0,0 +1,20 @@
+import WelcomeMessage from './components/WelcomeMessage';  
+return (  
+    <div>  
+        <WelcomeMessage />  
+        {/* Other components or content */}  
+    </div>  
+);  
+import React from 'react';  
+import WelcomeMessage from './components/WelcomeMessage';  
+
+function App() {  
+    return (  
+        <div>  
+            <WelcomeMessage />  
+            {/* You can include other content here */}  
+        </div>  
+    );  
+}  
+
+export default App;  

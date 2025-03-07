e583788cf39adec4251fadd2e103fe6624018d4c
Ahmed-Samy-2003 <aahmedsamy222@gmail.com>
2025-03-07 15:05:39 +0200

    Create App.jsx

---
+++ alx-react-app-props/App.jsx
@@ -0,0 +1,15 @@
+// src/App.jsx  
+import ProfilePage from './ProfilePage';  
+import UserContext, { UserProvider } from './UserContext';  
+
+function App() {  
+  const userData = { name: "Jane Doe", email: "jane.doe@example.com" };  
+
+  return (  
+    <UserProvider value={userData}>  
+      <ProfilePage />  
+    </UserProvider>  
+  );  
+}  
+
+export default App;  

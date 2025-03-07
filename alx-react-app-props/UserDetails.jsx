0897088058c84450025975a3151ad90ee06ecacb
Ahmed-Samy-2003 <aahmedsamy222@gmail.com>
2025-03-07 15:06:47 +0200

    Create UserDetails.jsx

---
+++ alx-react-app-props/UserDetails.jsx
@@ -0,0 +1,16 @@
+// src/UserDetails.jsx  
+import React, { useContext } from 'react';  
+import UserContext from './UserContext';  
+
+function UserDetails() {  
+  const userData = useContext(UserContext); // Consume the context  
+
+  return (  
+    <div>  
+      <p>Name: {userData.name}</p>  
+      <p>Email: {userData.email}</p>  
+    </div>  
+  );  
+}  
+
+export default UserDetails;  

bac9b2dfd01954a136308c814d31b3ff824a5513
Ahmed-Samy-2003 <aahmedsamy222@gmail.com>
2025-03-07 15:40:55 +0200

    Create App.js

---
+++ form-handling-react/src/App.js
@@ -0,0 +1,14 @@
+// src/App.js  
+import React from 'react';  
+import RegistrationForm from './RegistrationForm';  
+
+const App = () => {  
+  return (  
+    <div>  
+      <h1>User Registration</h1>  
+      <RegistrationForm />  
+    </div>  
+  );  
+};  
+
+export default App;  

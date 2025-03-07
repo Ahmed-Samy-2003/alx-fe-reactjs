3f8195dcc4e180feba1d14b21a7f9ba6f72cd432
Ahmed-Samy-2003 <aahmedsamy222@gmail.com>
2025-03-07 15:23:16 +0200

    Create main.jsx

---
+++ form-handling-react/main.jsx
@@ -0,0 +1,11 @@
+import React from 'react';  
+import ReactDOM from 'react-dom/client';  
+import App from './App';  
+import RegistrationForm from './components/RegistrationForm';  
+
+ReactDOM.createRoot(document.getElementById('root')).render(  
+    <React.StrictMode>  
+        <App />  
+        <RegistrationForm />  
+    </React.StrictMode>  
+);  

1c8affec99e8ac2b52df7791f5cbe017e1c85cea
Ahmed-Samy-2003 <aahmedsamy222@gmail.com>
2025-03-08 12:57:27 +0200

    Create Footer.jsx

---
+++ alx-react-app-new/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';  
+
+function Footer() {  
+    return (  
+        <footer style={{ backgroundColor: 'darkslategray', color: 'white', textAlign: 'center', padding: '10px' }}>  
+            <p>© 2025 My Favorite Cities. All rights reserved.</p>  
+        </footer>  
+    );  
+}  
+
+export default Footer;  

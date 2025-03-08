71d431b62d43cf2c71d5fa444620c789cde85fd5
Ahmed-Samy-2003 <aahmedsamy222@gmail.com>
2025-03-08 12:32:10 +0200

    Update App.jsx

--- my-company/src/App.jsx
+++ my-company/src/App.jsx
@@ -17,3 +17,23 @@ function App() {
 }  
 
 export default App;  
+import Navbar from './components/Navbar'; // تأكد من أن المسار صحيح  
+import React from 'react';  
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';  
+import Navbar from './components/Navbar'; // استيراد Navbar  
+import Home from './Home'; // تأكد من وجود مكون Home  
+import About from './About'; // تأكد من وجود مكون About  
+
+function App() {  
+  return (  
+    <Router>  
+      <Navbar />  {/* أضف Navbar هنا */}  
+      <Routes>  
+        <Route path="/" element={<Home />} />  
+        <Route path="/about" element={<About />} />  
+      </Routes>  
+    </Router>  
+  );  
+}  
+
+export default App;  

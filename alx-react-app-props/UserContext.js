fb06780efbcff07158f64d3907d9f5721b08fd50
Ahmed-Samy-2003 <aahmedsamy222@gmail.com>
2025-03-08 13:04:25 +0200

    Update UserContext.js

--- alx-react-app-props/UserContext.js
+++ alx-react-app-props/UserContext.js
@@ -4,3 +4,19 @@ import React, { createContext } from 'react';
 const UserContext = createContext();  
 
 export default UserContext;  
+// UserDetails.jsx  
+import React, { useContext } from 'react';  
+import UserContext from './UserContext';  
+
+function UserDetails() {  
+  const userData = useContext(UserContext);  
+
+  return (  
+    <div>  
+      <p>Name: {userData.name}</p>  
+      <p>Email: {userData.email}</p>  
+    </div>  
+  );  
+}  
+
+export default UserDetails;  

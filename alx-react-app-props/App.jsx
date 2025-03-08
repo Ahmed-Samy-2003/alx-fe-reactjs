fc1528cfce2ad6488e74b66dfe2ae90c46b8d6f6
Ahmed-Samy-2003 <aahmedsamy222@gmail.com>
2025-03-08 13:08:33 +0200

    Update App.jsx

--- alx-react-app-props/App.jsx
+++ alx-react-app-props/App.jsx
@@ -1,12 +1,13 @@
 // App.jsx  
 import React from 'react';  
-import ProfilePage from './ProfilePage';  
-import UserContext from './UserContext';  
+import ProfilePage from './ProfilePage'; // استيراد مكون ProfilePage  
+import UserContext from './UserContext'; // استيراد UserContext  
 
 function App() {  
-  const userData = { name: "Jane Doe", email: "jane.doe@example.com" };  
+  const userData = { name: "Jane Doe", email: "jane.doe@example.com" }; // بيانات المستخدم  
 
   return (  
+    // إحاطة مكون ProfilePage بمكون UserContext.Provider وتمرير userData كقيمة  
     <UserContext.Provider value={userData}>  
       <ProfilePage />  
     </UserContext.Provider>  

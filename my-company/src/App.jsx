6b4e50eda9a3191584ad8038ecc0a7f4a4b13c5a
Ahmed-Samy-2003 <aahmedsamy222@gmail.com>
2025-03-08 12:29:13 +0200

    Update App.jsx

--- my-company/src/App.jsx
+++ my-company/src/App.jsx
@@ -1,1 +1,19 @@
+import React from 'react';  
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import React from 'react';  
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';  
+import Home from './Home'; // تأكد من أن لديك مكون Home  
+import About from './About'; // تأكد من أن لديك مكون About  
 
+function App() {  
+  return (  
+    <Router>  
+      <Routes>  
+        <Route path="/" element={<Home />} />  
+        <Route path="/about" element={<About />} />  
+      </Routes>  
+    </Router>  
+  );  
+}  
+
+export default App;  

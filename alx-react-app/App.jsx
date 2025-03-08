c26e8ce9853a697a822850b9ef6166c172898e68
Ahmed-Samy-2003 <aahmedsamy222@gmail.com>
2025-03-08 11:28:03 +0200

    Delete alx-react-app/App.jsx

--- alx-react-app/App.jsx
+++
@@ -1,9 +0,0 @@
-import WelcomeMessage from './components/WelcomeMessage';  
-function App() {  
-    return (  
-        <div>  
-            <WelcomeMessage />  
-            {/* Any other components or content */}  
-        </div>  
-    );  
-}  

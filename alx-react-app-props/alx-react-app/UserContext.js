3fc4f09a9a63280848dd801cc758f507c7891456
Ahmed-Samy-2003 <aahmedsamy222@gmail.com>
2025-03-10 08:36:51 +0200

    Create UserContext.js

---
+++ alx-react-app-props/alx-react-app/UserContext.js
@@ -0,0 +1,8 @@
+// UserContext.js  
+import React, { createContext } from 'react';  
+
+// إنشاء السياق الجديد  
+const UserContext = createContext();  
+
+// تصدير السياق لاستخدامه في مكونات أخرى  
+export default UserContext;  

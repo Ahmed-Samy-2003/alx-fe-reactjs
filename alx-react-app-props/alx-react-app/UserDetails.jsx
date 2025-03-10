555fd1b4c3ed8a25a7c089f2d45bb86adec225e8
Ahmed-Samy-2003 <aahmedsamy222@gmail.com>
2025-03-10 08:41:26 +0200

    Update UserDetails.jsx

--- alx-react-app-props/alx-react-app/UserDetails.jsx
+++ alx-react-app-props/alx-react-app/UserDetails.jsx
@@ -3,14 +3,14 @@ import React, { useContext } from 'react';
 import UserContext from './UserContext'; // استيراد UserContext  
 
 function UserDetails() {  
-  const userData = useContext(UserContext); // استهلاك البيانات من السياق  
+    const userData = useContext(UserContext); // استهلاك السياق للحصول على بيانات المستخدم  
 
-  return (  
-    <div>  
-      <p>Name: {userData.name}</p> {/* عرض اسم المستخدم */}  
-      <p>Email: {userData.email}</p> {/* عرض البريد الإلكتروني */}  
-    </div>  
-  );  
+    return (  
+        <div>  
+            <p>Name: {userData.name}</p> {/* عرض اسم المستخدم */}  
+            <p>Email: {userData.email}</p> {/* عرض البريد الإلكتروني */}  
+        </div>  
+    );  
 }  
 
 export default UserDetails;  

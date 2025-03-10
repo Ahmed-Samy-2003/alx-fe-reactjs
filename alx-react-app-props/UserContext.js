f4f8de4e845a0c88985305c4498d80bd22dc034a
Ahmed-Samy-2003 <aahmedsamy222@gmail.com>
2025-03-10 08:29:40 +0200

    Update UserContext.js

--- alx-react-app-props/UserContext.js
+++ alx-react-app-props/UserContext.js
@@ -6,25 +6,3 @@ const UserContext = createContext();
 
 // تصدير الـ Context للاستخدام في مكونات أخرى  
 export default UserContext;  
-// بعض مكون React  
-import React, { useContext } from 'react';  
-import UserContext from './UserContext';  
-
-const UserProfile = () => {  
-    // استخدام الـ Context للحصول على بيانات المستخدم  
-    const userContext = useContext(UserContext);  
-    const { user } = userContext; // على افتراض أن لدينا مستخدمًا  
-
-    return (  
-        <div>  
-            <h1>User Profile</h1>  
-            {user ? (  
-                <p>Welcome, {user.name}!</p>  
-            ) : (  
-                <p>Please log in.</p>  
-            )}  
-        </div>  
-    );  
-};  
-
-export default UserProfile;  

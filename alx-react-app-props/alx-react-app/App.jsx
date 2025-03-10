6d102dee876c62b58ca689ba65b0af256bbb036e
Ahmed-Samy-2003 <aahmedsamy222@gmail.com>
2025-03-10 08:41:03 +0200

    Update App.jsx

--- alx-react-app-props/alx-react-app/App.jsx
+++ alx-react-app-props/alx-react-app/App.jsx
@@ -4,13 +4,13 @@ import ProfilePage from './ProfilePage';
 import UserContext from './UserContext'; // استيراد UserContext  
 
 function App() {  
-  const userData = { name: "Jane Doe", email: "jane.doe@example.com" }; // بيانات المستخدم  
+    const userData = { name: "Jane Doe", email: "jane.doe@example.com" }; // بيانات المستخدم  
 
-  return (  
-    <UserContext.Provider value={userData}> {/* توفير بيانات المستخدم للسياق */}  
-      <ProfilePage />  
-    </UserContext.Provider>  
-  );  
+    return (  
+        <UserContext.Provider value={userData}> {/* توفير بيانات المستخدم عبر السياق */}  
+            <ProfilePage />  
+        </UserContext.Provider>  
+    );  
 }  
 
 export default App;  

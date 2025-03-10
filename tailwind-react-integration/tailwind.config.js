6502816bee4e8ec5572e403ba1543014b7da5eff
Ahmed-Samy-2003 <aahmedsamy222@gmail.com>
2025-03-10 07:44:58 +0200

    Update tailwind.config.js

--- tailwind-react-integration/tailwind.config.js
+++ tailwind-react-integration/tailwind.config.js
@@ -1,1 +1,11 @@
-
+module.exports = {  
+  content: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"], // Updated purge property  
+  darkMode: false, // or 'media' or 'class'  
+  theme: {  
+    extend: {},  
+  },  
+  variants: {  
+    extend: {},  
+  },  
+  plugins: [],  
+};  

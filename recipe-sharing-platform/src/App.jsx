3b89fb56a01d58ce8d96873ef413a11e3b9a7889
Ahmed-Samy-2003 <aahmedsamy222@gmail.com>
2025-03-18 16:34:34 +0200

    Create App.jsx

---
+++ recipe-sharing-platform/src/App.jsx
@@ -0,0 +1,9 @@
+function App() {  
+  return (  
+    <div className="container mx-auto p-4">  
+      <h1 className="text-blue-500 text-2xl">Welcome to the Recipe Sharing Platform!</h1>  
+    </div>  
+  );  
+}  
+
+export default App;  

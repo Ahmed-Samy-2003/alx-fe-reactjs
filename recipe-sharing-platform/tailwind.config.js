5be5390d04fd5a0a58bfbf1a388bac4c76c8a8d1
Ahmed-Samy-2003 <aahmedsamy222@gmail.com>
2025-03-18 16:33:48 +0200

    Create tailwind.config.js

---
+++ recipe-sharing-platform/tailwind.config.js
@@ -0,0 +1,8 @@
+module.exports = {  
+  content: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],  
+  darkMode: false, // 'media' or 'class' can be set here for dark mode  
+  theme: {  
+    extend: {},  
+  },  
+  plugins: [],  
+};  

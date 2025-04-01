c329e10674b9af242b5173b2af4a5abc783ba429
Ahmed-Samy-2003 <aahmedsamy222@gmail.com>
2025-04-01 15:17:13 +0300

    Create App.jsx

---
+++ github-user-search/github-user-search/  src/components/services/App.jsx
@@ -0,0 +1,14 @@
+// src/App.jsx  
+import React from 'react';  
+import './App.css';  
+
+function App() {  
+  return (  
+    <div className="App">  
+      <h1>GitHub User Search</h1>  
+      {/* Add components for search input and results here */}  
+    </div>  
+  );  
+}  
+
+export default App;  

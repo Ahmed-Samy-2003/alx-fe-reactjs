0295dcdef6fc35f3c248678e106f0150b40f89ed
Ahmed-Samy-2003 <aahmedsamy222@gmail.com>
2025-04-01 15:52:34 +0300

    Update githubService.js

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -25,3 +25,11 @@ export const fetchUserData = async (username) => {
   const response = await axios.get(`https://api.github.com/users/${username}`);  
   return response.data; // Return user data  
 };  
+// src/services/githubService.js  
+import axios from 'axios';  
+
+// Function to fetch user data from GitHub API by username  
+export const fetchUserData = async (username) => {  
+  const response = await axios.get(`https://api.github.com/users/${username}`);  
+  return response.data; // Return the user data  
+};  
